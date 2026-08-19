1c7d05f2d044e37464a6ced2f2ab336d866480d5
HP <HP@LAPTOP-PBQJAFCL>
2026-08-19 10:06:42 +0700

    second commit

---
+++ Object Repository/LG006-For Looping/Page_e-Libs  TRUNOJOYO Library/a_Log in.rs
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Log in</name>
+   <tag></tag>
+   <elementGuidId>5df8c29a-f065-4755-9564-3e31ec9ffd4b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>[href=&quot;https\:\/\/library\.trunojoyo\.ac\.id\/elib\/loginSIAKAD\.php&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@href = 'https://library.trunojoyo.ac.id/elib/loginSIAKAD.php']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Log in&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>5c0e90c0-6f24-4df8-870a-86dd74f279ef</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://library.trunojoyo.ac.id/elib/loginSIAKAD.php</value>
+      <webElementGuid>df943a3e-dbca-4933-81ed-f84d22b7e450</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Log in</value>
+      <webElementGuid>67c74747-eea4-4059-8017-230644f0ed23</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>parent</name>
+      <type>Main</type>
+      <value>md5.v1-4aecaf62a1df829a6c6605afdd1e1ec6</value>
+      <webElementGuid>dfbf1f8e-ba74-4db9-9374-1ec30898cdb7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@href = 'https://library.trunojoyo.ac.id/elib/loginSIAKAD.php']</value>
+      <webElementGuid>8bc21a1f-6e8b-4bce-84a0-905045550a8a</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//*[@href = 'https://library.trunojoyo.ac.id/elib/loginSIAKAD.php']</value>
+      <webElementGuid>c694fd19-5824-4232-b78c-57a261a0856c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://library.trunojoyo.ac.id/elib/loginSIAKAD.php' and (text() = 'Log in' or . = 'Log in')]</value>
+      <webElementGuid>e2ed0195-d895-48ee-9eed-a44b02a6bdf6</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/LG006-For Looping/Page_e-Libs  TRUNOJOYO Library/a_Log out.rs
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Log out</name>
+   <tag></tag>
+   <elementGuidId>a3267a75-beac-41da-af99-0eabcffca782</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>[href=&quot;logout\.php&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@href = 'logout.php']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Log out&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>ebf7a676-0111-4310-9c09-be70e2675562</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>logout.php</value>
+      <webElementGuid>c1fd158e-4519-4c28-9437-fb803106c645</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Log out</value>
+      <webElementGuid>d205eb3c-f775-41b4-b8e9-d9fc2274da38</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>parent</name>
+      <type>Main</type>
+      <value>md5.v1-43aa4d08f9f275e8cca80f174f2e24fe</value>
+      <webElementGuid>d60713cf-0686-4a49-bfe8-16b5911c594b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@href = 'logout.php']</value>
+      <webElementGuid>71a51336-a269-48f7-92fd-31660734748d</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//*[@href = 'logout.php']</value>
+      <webElementGuid>c7eff4dd-e050-475e-be02-397f5d71484a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'logout.php' and (text() = 'Log out' or . = 'Log out')]</value>
+      <webElementGuid>2b5fffee-f835-4db3-8230-48864cda74ab</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
